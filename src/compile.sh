adb7d7b9b6d9e5d3edc0e02d315a5e508bb61f1e
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2016-11-28 13:58:22 +0100

    further code simplification

--- src/compile.sh
+++ src/compile.sh
@@ -1,5 +1,5 @@
 CC=gcc-6
-WFLAGS="-Wall -Wextra -Wno-unused -Wno-sign-compare"
+WFLAGS="-Wall -Wextra -Wno-sign-compare"
 OFLAGS="-O3 -ffast-math -march=native -mtune=native"
 CFLAGS="-std=gnu99 $WFLAGS $OFLAGS"
 OCVFLAGS=`pkg-config opencv --cflags --libs`
--- src/dataconv.c
+++ src/dataconv.c
@@ -1,94 +1,11 @@
-// BINARY DATA FORMATS
-// -------------------
-//
-// RAW: bytes, or "packed bits"
-// BIT: one bit on every byte, or "unpacked bits"
-// RLE1: bits encoded into bytes by lenght of constant runs
-// RLE8: run-length encoded bytes, à la PCX
-// HUF8: canonically Huffman-encoded bytes
-// HUF1: canonically Huffman-encoded runs of bits (i.e., RLE1 followed by HUF8)
-// B64: base 64 encoding of bytes into printable characters
-// A85: base 85 encoding of bytes into printable characters
-// X85: base 85 encoding of bytes into printable characters (alternate charset)
-// FAX: RLE1 + HUF8 + B64
-// LZW: Lempel-Ziv (not implemented)
-
-// implementation idea: do not think in terms of data conversions between pairs
-// of formats, but in terms of data transformations, from whatever format into
-// the target one.  That way, composition of formats is automatically supported.
-
-
-///////////////
-////  API  ////
-///////////////
-#include <stdint.h>
-
-
-// 1. elementary transformations
-
-// 1.1. unpacking and packing
-uint8_t *alloc_and_transform_from_RAW_to_BIT(uint8_t *x, int n, int *nout);
-uint8_t *alloc_and_transform_from_BIT_to_RAW(uint8_t *x, int n, int *nout);
-
-// 1.2. run-length encoding and decoding
-uint8_t *alloc_and_transform_from_BIT_to_RLE1(uint8_t *x, int n, int *nout);
-uint8_t *alloc_and_transform_from_RLE1_to_BIT(uint8_t *x, int n, int *nout);
-uint8_t *alloc_and_transform_from_RAW_to_RLE8(uint8_t *x, int n, int *nout);
-uint8_t *alloc_and_transform_from_RLE8_to_RAW(uint8_t *x, int n, int *nout);
-
-// 1.3. differential coding and decoding
-uint8_t *alloc_and_transform_diff(uint8_t *x, int n, int *nout);
-uint8_t *alloc_and_transform_undiff(uint8_t *x, int n, int *nout);
-uint8_t *alloc_and_transform_xor(uint8_t *x, int n, int *nout);
-
-// 1.3. canonical huffman encoding and decoding
-uint8_t *alloc_and_transform_from_RAW_to_HUF8(uint8_t *x, int n, int *nout);
-uint8_t *alloc_and_transform_from_HUF8_to_RAW(uint8_t *x, int n, int *nout);
-
-// 1.4. base64 encoding
-uint8_t *alloc_and_transform_from_RAW_to_B64(uint8_t *x, int n, int *nout);
-uint8_t *alloc_and_transform_from_B64_to_RAW(uint8_t *x, int n, int *nout);
-
-// 1.5. base85 encoding
-uint8_t *alloc_and_transform_from_RAW_to_A85(uint8_t *x, int n, int *nout);
-uint8_t *alloc_and_transform_from_A85_to_RAW(uint8_t *x, int n, int *nout);
-
-// 1.6. lempel-ziv compression (will not implemented)
-//uint8_t *alloc_and_transform_from_RAW_to_LZW(uint8_t *x, int n, int *nout);
-
-
-
-// 2. composite transformations (as a form of API convenience)
-
-// 2.1. BIT => RLE1 => HUF8
-uint8_t *alloc_and_transform_from_BIT_to_HUF1(uint8_t *x, int n, int *nout);
-uint8_t *alloc_and_transform_from_HUF1_to_BIT(uint8_t *x, int n, int *nout);
-
-// 2.2. RAW => BIT => RLE1 => HUF8
-uint8_t *alloc_and_transform_from_RAW_to_HUF1(uint8_t *x, int n, int *nout);
-uint8_t *alloc_and_transform_from_HUF1_to_RAW(uint8_t *x, int n, int *nout);
-
-// 2.3. BIT => RLE1 => HUF8 => B64
-uint8_t *alloc_and_transform_from_BIT_to_B64(uint8_t *x, int n, int *nout);
-uint8_t *alloc_and_transform_from_B64_to_BIT(uint8_t *x, int n, int *nout);
-
-// 2.4. RAW => BIT => RLE1 => HUF8 => B64
-uint8_t *alloc_and_transform_from_RAW_to_FAX(uint8_t *x, int n, int *nout);
-uint8_t *alloc_and_transform_from_FAX_to_RAW(uint8_t *x, int n, int *nout);
-
-
-
-//////////////////////////
-////  IMPLEMENTATION  ////
-//////////////////////////
-
+// simple data conversion routines
 #include <assert.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdlib.h>
 
 #include "xmalloc.c"
-#include "fail.c"
 
 #define SETBIT(x,i) ((x)|=(1<<(i)))
 #define GETBIT(x,i) (bool)((x)&(1<<(i)))
